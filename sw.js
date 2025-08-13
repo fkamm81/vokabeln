f207c8bb7eece8136dca254089530f135e63371e
fkamm81 <ferdinandkammerer@gmx.de>
2025-08-13 09:18:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
-const CACHE='vocab-pwa-release-v1';
-const ASSETS=['./','./index.html','./styles.css?v=rel1','./app.js?v=rel1','./manifest.json','./assets/mascots.svg'];
+const CACHE='vocab-pwa-release2';
+const ASSETS=['./','./index.html','./styles.css?v=r2','./app.js?v=r2','./manifest.json','./assets/mascots.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k))))); self.clients.claim();});
 self.addEventListener('fetch',e=>{
